8006df4d822d4186d03f770e3a6f6b824c124ca5
dezur <petrenyi@gmail.com>
2021-04-29 11:45:02 +0200

    Task completed

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,29 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        try {
+            if(contacts == null) {
+                throw new IllegalArgumentException();
+            }
+            File file = new File(output);
+
+            BufferedWriter bf = new BufferedWriter(new FileWriter(file));
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                bf.write(entry.getKey() + ": " + entry.getValue());
+                bf.newLine();
+            }
+            bf.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
